d679d495cecbdcf1d7fae7629e836673008723d3
yaswanth-vempuluru <yaswanthvempuluru@outlook.com>
2024-09-02 19:03:30 +0530

    built-in

---
+++ Built_In/max_min.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() 
+{
+    cout<<"INT_MAX : "<<INT_MAX<<endl;
+    cout<<"INT_MIN : "<<INT_MIN<<endl;
+    cout<<"FLT_MAX : "<<FLT_MAX<<endl;
+    cout<<"FLT_MIN : "<<FLT_MIN<<endl;
+    cout<<"DBL_MAX : "<<DBL_MAX<<endl;
+    cout<<"DBL_MIN : "<<DBL_MIN<<endl;
+    cout<<"LONG_MAX : "<<LONG_MAX<<endl;
+    cout<<"LONG_MIN : "<<LONG_MIN<<endl;
+    cout<<"LLONG_MAX : "<<LLONG_MAX<<endl;
+    cout<<"LLONG_MIN : "<<LLONG_MIN<<endl;
+    
+    cout<<numeric_limits<long>::max()<<endl;
+    
+    
+ 
+}
